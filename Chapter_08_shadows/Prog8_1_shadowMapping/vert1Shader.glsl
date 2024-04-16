d632ea8a43637a250609533013f5556bd512ee4f
maktub <leon_zf@icloud.com>
2024-04-16 09:02:44 +0800

    init project

---
+++ Chapter_08_shadows/Prog8_1_shadowMapping/vert1Shader.glsl
@@ -0,0 +1,9 @@
+#version 410
+
+layout (location=0) in vec3 vertPos;
+
+uniform mat4 shadowMVP;
+
+void main(void)
+{	gl_Position = shadowMVP * vec4(vertPos,1.0);
+}
---
+++ Chapter_12_tessellation/Prog12_1_tessellationGridOnly/vertShader.glsl
@@ -0,0 +1,7 @@
+#version 410
+
+uniform mat4 mvp_matrix;
+
+void main(void)
+{
+}
